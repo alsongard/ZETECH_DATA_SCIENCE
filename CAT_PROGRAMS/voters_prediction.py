619b066fd1070cdb1163b9daee6678b2f3d75677
alsongard <alsongard@gmail.com>
2024-03-22 12:26:16 -0400

    created new file and using handler column

--- CAT_PROGRAMS/voters_prediction.py
+++ CAT_PROGRAMS/voters_prediction.py
@@ -4,6 +4,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report
 from IPython.display import display
 from sklearn.naive_bayes import MultinomialNB
+import urllib.request
+
+# urllib.request.urlretrieve("/kaggle/input/us-election-2020-tweets/hashtag_donaldtrump.csv", "../myData/hashtagtrump.csv")#not working link error
 data_df = pd.read_csv("../myData/tweets.csv")
 print(data_df.head(15))
 
@@ -23,9 +26,16 @@ data_df = data_df[["id", "handle", "text"]]
 #drop null values 
 #the dropna will drop any row containing null values
 data_df = data_df.dropna()
-#checking for null values
 
+#checking for null values
 print(data_df.isnull())
+
+#write to another file
+data_df.head(3400).to_csv("../myData/twitterElectionData.csv",index=False)
+
+#read again the data
+election_df = pd.read_csv("../myData/twitterElectionData.csv")
+print(election_df.head(20).handle)
 #filter only the required columnss
 
 
